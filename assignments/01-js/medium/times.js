95438cdd739bc65c2627088b0e89ebcee7294ab5
ekamsinghahuja <ekamsinghahuja123@gmail.com>
2024-08-07 22:07:44 +0530

    assignments

--- assignments/01-js/medium/times.js
+++ assignments/01-js/medium/times.js
@@ -9,5 +9,17 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let startTime = new Date().getTime();
+    let cnt = 0;
+    for(let i=0;i<n;i++){
+        cnt += (i+1)
+    }
+    let endTime = new Date().getTime();
+
+    // Calculate the time taken in seconds
+    let timeTaken = (endTime - startTime) / 1000;
+
+    return timeTaken;
+    
+    
 }
